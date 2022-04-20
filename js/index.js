83774ab7d0e6cf4d0cb3e3b14a99722c8eb5d5ff
Filipi Rafael <filipirafael@MacBook-Air-de-Filipi.local>
2022-04-19 23:17:26 -0300

    Add: Operador de igual a validação de altura e peso

--- js/index.js
+++ js/index.js
@@ -2,13 +2,13 @@ let pacientePrimeiro = document.querySelector("#primeiro-paciente");
 let pesoPrimeiro = Number(pacientePrimeiro.querySelector(".info-peso").textContent);
 let alturaPrimeiro = Number(pacientePrimeiro.querySelector(".info-altura").textContent);
 
-if (pesoPrimeiro < 0 || pesoPrimeiro > 1000) {
+if (pesoPrimeiro <= 0 || pesoPrimeiro >= 1000) {
     celula = pacientePrimeiro.querySelector(".info-imc");
     celula.textContent = "Peso inválido!";
     celula.style.color = "red";
     celula.style.textTransform = "uppercase";
     celula.style.fontWeight = "bold";
-} else if (alturaPrimeiro > 3 || alturaPrimeiro < 0) {
+} else if (alturaPrimeiro >= 3 || alturaPrimeiro <= 0) {
     celula = pacientePrimeiro.querySelector(".info-imc");
     celula.textContent = "Altura inválida!";
     celula.style.color = "red";
