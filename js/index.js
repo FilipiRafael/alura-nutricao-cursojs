f1bf33e255f8bd676301cc28aba2fd38bcdf528f
Filipi Rafael <filipirafael@MacBook-Air-de-Filipi.local>
2022-04-19 23:09:28 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let pacientePrimeiro = document.querySelector("#primeiro-paciente");
+let pesoPrimeiro = Number(pacientePrimeiro.querySelector(".info-peso").textContent);
+let alturaPrimeiro = Number(pacientePrimeiro.querySelector(".info-altura").textContent);
+
+if (pesoPrimeiro < 0 || pesoPrimeiro > 1000) {
+    celula = pacientePrimeiro.querySelector(".info-imc");
+    celula.textContent = "Peso inválido!";
+    celula.style.color = "red";
+    celula.style.textTransform = "uppercase";
+    celula.style.fontWeight = "bold";
+} else if (alturaPrimeiro > 3 || alturaPrimeiro < 0) {
+    celula = pacientePrimeiro.querySelector(".info-imc");
+    celula.textContent = "Altura inválida!";
+    celula.style.color = "red";
+    celula.style.textTransform = "uppercase";
+    celula.style.fontWeight = "bold";
+} else {
+    calculaImc = (peso, altura) => {
+        imc = peso / (altura * altura)
+        return imc
+    }
+}
+
+
+let resultadoPrimeiroPaciente = calculaImc(pesoPrimeiro, alturaPrimeiro);
+
+pacientePrimeiro.querySelector(".info-imc").innerHTML = resultadoPrimeiroPaciente;
